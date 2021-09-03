2d4eefc45f3a86b2d20a94865a55a36fdbb36750
kappekoff <47227587+kappekoff@users.noreply.github.com>
2021-09-03 12:39:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $x = "1";
+      $y = "2";
+      echo $x.$y;
+     ?>
+  </body>
+</html>
