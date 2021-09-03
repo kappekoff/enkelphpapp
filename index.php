b36df8aedb2c30b75ea5d176ef7e0aa3848348e7
kappekoff <47227587+kappekoff@users.noreply.github.com>
2021-09-03 13:13:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     echo "kom hit";
       // PHP Data Objects(PDO) Sample Code:
       try {
-          $conn = new PDO("sqlsrv:server = tcp:steinang.database.windows.net,1433; Database = Bibliotek", "steinang", "{your_password_here}");
+          $conn = new PDO("sqlsrv:server = tcp:steinang.database.windows.net,1433; Database = Bibliotek", "steinang", "247jeykS");
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       }
       catch (PDOException $e) {
