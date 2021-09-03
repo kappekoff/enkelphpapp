764e36d81ba35141cf4ef6cda2acb9a2e6c185e4
kappekoff <47227587+kappekoff@users.noreply.github.com>
2021-09-03 13:11:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
   </head>
   <body>
     <?php
-      $x = "1";
-      $y = "2";
-      echo $x.$y;
-     ?>
+      // PHP Data Objects(PDO) Sample Code:
+      try {
+          $conn = new PDO("sqlsrv:server = tcp:steinang.database.windows.net,1433; Database = Bibliotek", "steinang", "{your_password_here}");
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $e) {
+          print("Error connecting to SQL Server.");
+          die(print_r($e));
+      }
+    ?>
   </body>
 </html>
